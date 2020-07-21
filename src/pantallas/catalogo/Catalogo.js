5a061ae77342a2e242df0f325cdef0472dd95563
Emanuel Ospino Gutierrez <oemanuel@uninorte.edu.co>
2020-07-21 05:25:29 -0500

    [modal horario][listo][oemanuel][21072020]

--- src/pantallas/catalogo/Catalogo.js
+++ src/pantallas/catalogo/Catalogo.js
@@ -1,16 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, Image, TouchableOpacity, StatusBar} from 'react-native';
 import styles from './styles';
-import BarraBusqueda from '../../componentes/BarraBusqueda';
-import Informacion from '../../componentes/InformacionCatalogo';
-import ContenedorCategoria from '../../componentes/ContenedorCategoria';
-import ContenedorProducto from '../../componentes/ContenedorProducto';
 import ListaDeProductos from '../../componentes/ListaDeProductos';
 import Menu from '../menu/Menu';
+import Horario from '../../componentes/Horario';
 import {ScrollView, FlatList} from 'react-native-gesture-handler';
 
 const Catalogo = ({navigation}) => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [horario, setHorario] = useState(true);
   return (
     <>
       <StatusBar
@@ -24,6 +22,7 @@ const Catalogo = ({navigation}) => {
         visible={menuVisible}
         setMenuVisible={setMenuVisible}
       />
+      <Horario visible={horario} />
       <View style={styles.fondo}>
         <View style={styles.header}>
           <TouchableOpacity
